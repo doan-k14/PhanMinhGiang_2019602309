a02fac67341867a7eaf4196f99307fec3c0f9316
giangalan <ganalanphan@gmail.com>
2023-05-08 23:18:34 +0700

    Update for Model

--- Train_Model/W2V_model/test_model.py
+++ Train_Model/W2V_model/test_model.py
@@ -1,34 +1,15 @@
-from gensim.models import KeyedVectors
-# from sklearn.decomposition import PCA
+from using_model import WordEmbedding
 import numpy as np
 
-model = KeyedVectors.load('word_embedding.model')
-vec_dict={}
-def word2vec(word, model):
-    vec = model.wv[word]
-    return (vec)
+model_vec_dict={}
+we = WordEmbedding(model_vec_dict=model_vec_dict)
 
-# def sent2vec(sentence, model):
-#     sentence = sentence.lower().split()
-#     vec = [model.wv[word] for word in sentence if word in model]
-#     return np.mean(vec, axis=0)
+sents = ['Có một chú chó trong nhà tôi', 'Nhà tôi có 2 con mèo', 'Hai con mèo chơi cùng hai con chó']
+vecs = [we.sent2vec(sent) for sent in sents]
+sents2 = [we.vec2sent(vec) for vec in vecs]
+print(sents2)
 
-
-# def vec2sent(vector, model):
-#     sim_words = model.similar_by_vector(vector, topn=6)
-#     words = [word[0] for word in sim_words]
-#     return ' '.join(words)
-
-
-sent = 'chó'
-vec = word2vec(sent, model)
-sent2 = model.wv.similar_by_vector(vec, topn=1)
-# print(vec)
-print(sent2)
-
-# sent2 = vec2sent(vec, model)
-# print (sent2)
-# word = 'chó'
-# vec = word2vec(word, model)
-# word2 = model.similar_by_vector(vec)
-# print(word2)
+paragraph = 'Có một chú hổ trong nhà tôi. Nhà tôi có 3 con chó. Hai con chó chơi cùng hai con hổ.'
+we.para2vecdict(paragraph)
+vec_list = [value for key,value in model_vec_dict.items()]
+print(we.veclist2para(vec_list))
--- Train_Model/W2V_model/word_embedding.py
+++ Train_Model/W2V_model/word_embedding.py
@@ -1,7 +1,3 @@
-import os
-import pandas as pd
-import string
-from pyvi import ViTokenizer
 from gensim.models import Word2Vec
 
 # path data
@@ -19,4 +15,4 @@ if __name__ == '__main__':
     train_data = read_data(pathdata)
 
     model = Word2Vec(train_data, vector_size=150, window=10, min_count=2, workers=4, sg=0)
-    model.save("word_embedding.model")
+    model.save("../Models/W2V/word_embedding.model")
